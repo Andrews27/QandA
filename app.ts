9aa8ca4495d6f0f3cd19c5d73383a95d35f0ab7a
jdflemin <jdf@mchsi.com>
2017-05-18 10:10:50 -0700

    updated services

--- app.ts
+++ app.ts
@@ -18,6 +18,12 @@ mongoose.connect(process.env.CONNECTION_STRING)
 
 let app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
